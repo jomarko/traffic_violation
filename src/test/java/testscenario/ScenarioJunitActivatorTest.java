b05440611ed9585d40383e1a266bab15a711b217
Jozef Marko <jozef.marko@ibm.com>
2025-04-11 14:57:49 +0200

    Allow to ru Traffic Violation scesim BAMOE9 - apache community version

--- src/test/java/testscenario/ScenarioJunitActivatorTest.java
+++ src/test/java/testscenario/ScenarioJunitActivatorTest.java
@@ -2,6 +2,6 @@ package testscenario;
 /**
 * Do not remove this file
 */
-@org.junit.runner.RunWith(org.drools.scenariosimulation.backend.runner.ScenarioJunitActivator.class)
+@org.junit.runner.RunWith(org.kogito.scenariosimulation.runner.KogitoJunitActivator.class)
 public class ScenarioJunitActivatorTest {
 }
